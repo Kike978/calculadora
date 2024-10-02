a15c40b84097c2a91f54d1f9ae889a6a91e39e17
Kike978 <zx23student3276@colegiovalledelmiro.es>
2024-10-02 09:15:44 +0200

    Añadir funcionalidad de dividir en la rama fature1

--- calculadora.py
+++ calculadora.py
@@ -4,6 +4,15 @@ def sumar(a, b):
 def restar(a, b):
     return a - b
 
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: División por cero"
+    return a / b
+
 print(sumar(3, 5))
 print(restar(8, 2))
-
+print(multiplicar(4, 3))
+print(dividir(10, 2))
